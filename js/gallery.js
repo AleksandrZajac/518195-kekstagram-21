6f26846b516a676924edca14a962fef47cbd1368
Aleksandr Zajac <mr.goodalex@gmail.com>
2020-10-28 18:32:37 +0200

    draft1

--- js/gallery.js
+++ js/gallery.js
@@ -30,10 +30,6 @@
       const photo = renderPhoto(itemsList[i]);
       photo.addEventListener(`click`, () => {
         window.picture.show(itemsList[i]);
-        commentsLoader.addEventListener(`click`, () => {
-          console.log(itemsList[i]);
-          window.picture.uploadComments(itemsList[i]);
-        });
       });
       fragment.append(photo);
     }
@@ -52,6 +48,6 @@
     document.body.insertAdjacentElement(`afterbegin`, node);
   };
 
-  window.load(successHandler, errorHandler);
+  window.http(successHandler, errorHandler);
 
 })();
--- js/preview.js
+++ js/preview.js
@@ -29,10 +29,10 @@
   const effectList = body.querySelector(`.effects__list`);
   const imgUploadPreview = body.querySelector(`.img-upload__preview img`);
   let effectTypes = {};
-  const main = document.querySelector(`main`);
 
   const openPopup = () => {
     window.photo.uploadPhoto();
+    console.log(window.photo.uploadPhoto());
     imgUploadOverlay.classList.remove(`hidden`);
     viewEffectLevel.classList.add(`hidden`);
     imgUploadPreview.style.filter = `none`;
@@ -192,86 +192,8 @@
     document.removeEventListener(`mouseup`, onMouseUp);
   };
 
-  const clearForm = () => {
-    window.photo.fileChooser.value = ``;
-    window.validate.hashtagInput.value = ``;
-    window.validate.comments.value = ``;
+  window.preview = {
+    closePopup
   };
-
-  const errorMes = [`#error`, `.error`];
-  const successMes = [`#success`, `.success`];
-
-  const createTemplate = (id, className) => {
-    const template = document.querySelector(id)
-      .content.
-      querySelector(className);
-    const element = template.cloneNode(true);
-    main.append(element);
-    const error = document.querySelector(className);
-    error.classList.add(`hidden`);
-    return error;
-  };
-  createTemplate(errorMes[0], errorMes[1]);
-  createTemplate(successMes[0], successMes[1]);
-
-  const error = document.querySelector(`.error`);
-  const errorButtonClose = document.querySelector(`.error__button`);
-  const success = document.querySelector(`.success`);
-  const successButtonClose = document.querySelector(`.success__button`);
-  const successInner = document.querySelector(`.success__inner`);
-  const errorInner = document.querySelector(`.error__inner`);
-
-  const successMessage = () => {
-    success.classList.remove(`hidden`);
-  };
-
-  const errorMessage = () => {
-    error.classList.remove(`hidden`);
-  };
-
-  const closeSuccessMessage = () => {
-    success.classList.add(`hidden`);
-  };
-
-  const closeErrorMessage = () => {
-    error.classList.add(`hidden`);
-  };
-
-  const form = document.querySelector(`.img-upload__form`);
-
-  const successHandler = () => {
-    closePopup();
-    clearForm();
-    successMessage();
-    successButtonClose.addEventListener(`click`, () => {
-      closeSuccessMessage();
-    });
-    success.addEventListener(`click`, (evtSuccess) => {
-      if (evtSuccess.target !== successInner) {
-        closeSuccessMessage();
-      }
-    });
-  };
-
-  let errorHandler = (errors) => {
-    const errorTitle = document.querySelector(`.error__title`);
-    errorTitle.textContent = errors;
-    closePopup();
-    clearForm();
-    errorMessage();
-    errorButtonClose.addEventListener(`click`, () => {
-      closeErrorMessage();
-    });
-    error.addEventListener(`click`, (evtSuccess) => {
-      if (evtSuccess.target !== errorInner) {
-        closeErrorMessage();
-      }
-    });
-  };
-
-  form.addEventListener(`submit`, (evt) => {
-    window.upload(new FormData(form), successHandler, errorHandler);
-    evt.preventDefault();
-  });
 
 })();
